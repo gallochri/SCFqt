bc79a76a587a6fe7918a989426296ba8f5670eca
Christian Galeffi <chri@gallochri.com>
2017-04-21 13:45:06 +0200

    qApp information

--- SCFqt.pro
+++ SCFqt.pro
@@ -11,16 +11,20 @@ greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 TARGET = SCFqt
 TEMPLATE = app
 
+DEFINES += QT_DEPRECATED_WARNINGS
 
 SOURCES += main.cpp\
         mainwindow.cpp \
-    datalistini.cpp
+        datalistini.cpp
 
 HEADERS  += mainwindow.h \
-    datalistini.h
+            datalistini.h
 
 FORMS    += mainwindow.ui
 
-CONFIG += mobility
-MOBILITY = 
+DISTFILES += \
+    .gitignore
 
+DEFINES += APP_NAME=\\\"SCFqt\\\"
+VERSION = 0.0.2
+DEFINES += APP_VERSION=\\\"$$VERSION\\\"
--- main.cpp
+++ main.cpp
@@ -7,6 +7,11 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+    qApp->setApplicationVersion(APP_VERSION);
+    qApp->setApplicationName(QString(APP_NAME).replace("_"," "));
+    qApp->setOrganizationName("gallochri.com");
+    qApp->setOrganizationDomain("www.gallochri.com");
+
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
     db.setHostName("freebsdserver");
     db.setDatabaseName("metel");
@@ -17,7 +22,7 @@ int main(int argc, char *argv[])
         qDebug() << "DB connection successfull";
         db.close();
         MainWindow *mainWin = new MainWindow;
-        mainWin->setWindowTitle (QObject::tr("SCF QT"));
+        mainWin->setWindowTitle (QString(APP_NAME).replace("_"," "));
         mainWin->show();
     } else {
         qDebug() << "Problem with DB connection";
