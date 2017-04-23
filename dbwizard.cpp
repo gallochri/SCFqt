e400fcce8a2efb69f8eb75ae0491c40fd853c76e
Christian Galeffi <chri@gallochri.com>
2017-04-23 22:00:38 +0200

    QWizard pages terminated

--- dbwizard.cpp
+++ dbwizard.cpp
@@ -1,12 +1,12 @@
 #include "dbwizard.h"
-
+#include <QtWidgets>
 #include <QBoxLayout>
 
 DbWizard::DbWizard(QWidget *parent) : QWizard(parent)
 {
     addPage(new IntroPage);
-//    addPage(new DbInfoPage);
-//    addPage(new ConclusionPage);
+    addPage(new DbConfigPage);
+    addPage(new ConclusionPage);
 
     setPixmap(QWizard::BannerPixmap, QPixmap(":/images/banner.jpg"));
 
@@ -28,15 +28,72 @@ IntroPage::IntroPage(QWidget *parent)
     setTitle(tr("Introduction"));
     setPixmap(QWizard::WatermarkPixmap, QPixmap(":/images/watermark.jpg"));
 
-    label = new QLabel(tr("This wizard will generate a skeleton C++ class "
-                          "definition, including a few functions. You simply "
-                          "need to specify the class name and set a few "
-                          "options to produce a header file and an "
-                          "implementation file for your new C++ class."));
+    label = new QLabel(tr("It seems that the database configuration does not exist"
+                          " or is incorrect. "
+                          "Fill in the fields on the next tab to retry the connection"
+                          " to the database."));
     label->setWordWrap(true);
 
     QVBoxLayout *layout = new QVBoxLayout;
     layout->addWidget(label);
     setLayout(layout);
+}
+
+DbConfigPage::DbConfigPage(QWidget *parent):QWizardPage(parent){
+    setTitle(tr("Database configuration"));
+    setSubTitle(tr("Specify information on how to connect to db"));
+    setPixmap(QWizard::LogoPixmap, QPixmap(":/images/icon.jpg"));
+
+    hostnameLabel = new QLabel(tr("&Hostname:"));
+    hostnameLineEdit = new QLineEdit;
+    hostnameLabel->setBuddy(hostnameLineEdit);
+    hostnameLineEdit->setPlaceholderText("www.example.com or 192.168.10.1");
+
+    databaseLabel = new QLabel(tr("&Database:"));
+    databaseLineEdit = new QLineEdit;
+    databaseLabel->setBuddy(databaseLabel);
+    databaseLineEdit->setPlaceholderText("MyDB");
+
+    usernameLabel = new QLabel(tr("&User name:"));
+    usernameLineEdit = new QLineEdit;
+    usernameLabel->setBuddy(usernameLabel);
+
+    passwordLabel = new QLabel(tr("&Password:"));
+    passwordLineEdit = new QLineEdit;
+    passwordLineEdit->setEchoMode(QLineEdit::Password);
+    passwordLabel->setBuddy(passwordLabel);
+
+    QGridLayout *layout = new QGridLayout;
+    layout->addWidget(hostnameLabel, 0,0);
+    layout->addWidget(hostnameLineEdit,0,1);
+    layout->addWidget(databaseLabel,1,0);
+    layout->addWidget(databaseLineEdit,1,1);
+    layout->addWidget(usernameLabel,2,0);
+    layout->addWidget(usernameLineEdit,2,1);
+    layout->addWidget(passwordLabel,3,0);
+    layout->addWidget(passwordLineEdit,3,1);
+    setLayout(layout);
+}
+
+ConclusionPage::ConclusionPage(QWidget *parent)
+    : QWizardPage(parent)
+{
+    setTitle(tr("Conclusion"));
+    setPixmap(QWizard::WatermarkPixmap, QPixmap(":/images/watermark.jpg"));
+
+    label = new QLabel;
+    label->setWordWrap(true);
+
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(label);
+    setLayout(layout);
+}
+
+void ConclusionPage::initializePage()
+{
+    QString finishText = wizard()->buttonText(QWizard::FinishButton);
+    finishText.remove('&');
+    label->setText(tr("Click %1 to generate the class skeleton.")
+                   .arg(finishText));
 }
 
--- dbwizard.h
+++ dbwizard.h
@@ -4,6 +4,7 @@
 #include <QWidget>
 #include <QWizard>
 #include <QLabel>
+#include <QLineEdit>
 
 class DbWizard : public QWizard
 {
@@ -25,5 +26,37 @@ private:
     QLabel *label;
 };
 
+class DbConfigPage : public QWizardPage{
+    Q_OBJECT
+public:
+    DbConfigPage(QWidget *parent = 0);
+private:
+    QLabel *hostnameLabel;
+    QLineEdit *hostnameLineEdit;
+
+    QLabel *databaseLabel;
+    QLineEdit *databaseLineEdit;
+
+    QLabel *usernameLabel;
+    QLineEdit *usernameLineEdit;
+
+    QLabel *passwordLabel;
+    QLineEdit *passwordLineEdit;
+
+};
+
+class ConclusionPage : public QWizardPage
+{
+    Q_OBJECT
+
+public:
+    ConclusionPage(QWidget *parent = 0);
+
+protected:
+    void initializePage() override;
+
+private:
+    QLabel *label;
+};
 
 #endif // DBWIZARD_H
