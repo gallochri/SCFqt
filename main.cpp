2a0828cc8a8858aeb3e63b8f1b534102f01a9a7d
Christian Galeffi <chri@gallochri.com>
2017-04-23 19:00:33 +0200

    QWizard dialog centered

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include "mainwindow.h"
+#include "dbwizard.h"
+
 #include <QApplication>
+#include <QSettings>
+#include <QSqlDatabase>
+#include <QDebug>
+#include <QDialog>
+#include <QDesktopWidget>
+
+#define KEY "db_connection"
+#define KEY_HOST "hostname"
+#define KEY_DB "database"
+#define KEY_USER "username"
+#define KEY_PWD "password"
 
 int main(int argc, char *argv[])
 {
@@ -14,5 +27,37 @@ int main(int argc, char *argv[])
     mainWin->setWindowTitle (QString(APP_NAME).replace("_"," "));
     mainWin->show();
 
+    //Read qsettings file for DB connection
+    QSettings settings;
+    settings.beginGroup(KEY);
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName(settings.value(KEY_HOST).toString());
+    db.setDatabaseName(settings.value(KEY_DB).toString());
+    db.setUserName(settings.value(KEY_USER).toString());
+    db.setPassword(settings.value(KEY_PWD).toString());
+    settings.endGroup();
+
+    // TODO write down a testing config file
+    settings.beginGroup(KEY);
+    settings.setValue(KEY_HOST, "freebsdserver2");
+    settings.setValue(KEY_DB, "testdb");
+    settings.setValue(KEY_USER, "gallochri");
+    settings.setValue(KEY_PWD, "password");
+    settings.endGroup();
+
+    //Test DB connection
+    if (!db.open()){
+        qDebug() << "Problem with DB connection";
+
+        //Start DB config wizard
+        DbWizard *wizard = new DbWizard(mainWin);
+        //Center the wizard dialog
+        QSize mSize = wizard->sizeHint();
+        const QRect screenRect = QApplication::desktop()->screenGeometry();
+        wizard->move(QPoint(screenRect.width()/3 - mSize.width()/2,
+                              screenRect.height()/2 - mSize.height()/2 ) );
+        wizard->show();
+    }
+    db.close();
     return app.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,51 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "dbwizard.h"
-
-#include <QDebug>
-
-#include <QSettings>
-#include <QSqlDatabase>
-#include <QInputDialog>
-
-#define KEY "db_connection"
-#define KEY_HOST "hostname"
-#define KEY_DB "database"
-#define KEY_USER "username"
-#define KEY_PWD "password"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QSettings settings;
-    settings.beginGroup(KEY);
-
-    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-    db.setHostName(settings.value(KEY_HOST).toString());
-    db.setDatabaseName(settings.value(KEY_DB).toString());
-    db.setUserName(settings.value(KEY_USER).toString());
-    db.setPassword(settings.value(KEY_PWD).toString());
-
-    settings.endGroup();
-    // Testing code
-    settings.beginGroup(KEY);
-    settings.setValue(KEY_HOST, "freebsdserver2");
-    settings.setValue(KEY_DB, "testdb");
-    settings.setValue(KEY_USER, "gallochri");
-    settings.setValue(KEY_PWD, "password");
-    settings.endGroup();
-
-    if (!db.open()){
-            qDebug() << "Problem with DB connection";
-            DbWizard *wizard = new DbWizard;
-            wizard->show();
-        }
-    qDebug() << "DB connection successfull";
-    db.close();
     setupView();
-
 }
 
 MainWindow::~MainWindow()
