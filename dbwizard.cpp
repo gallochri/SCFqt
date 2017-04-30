681074c9ff0db151c24fcc17211b1c8c99f887b3
Christian Galeffi <chri@gallochri.com>
2017-04-30 16:46:57 +0200

    Display old config in dbwizard

--- dbwizard.cpp
+++ dbwizard.cpp
@@ -56,23 +56,42 @@ DbConfigPage::DbConfigPage(QWidget *parent):QWizardPage(parent){
     setSubTitle(tr("Specify information on how to connect to db"));
     setPixmap(QWizard::LogoPixmap, QPixmap(":/images/logo.png"));
 
+    //Read qsettings file for DB connection
+    Config conf;
+    QString host = conf.loadConfig(KEY_HOST);
+    QString dbs = conf.loadConfig(KEY_DB);
+    QString user = conf.loadConfig(KEY_USER);
+    QString pass = conf.loadConfig(KEY_PWD);
+
     hostnameLabel = new QLabel(tr("&Hostname:"));
     hostnameLineEdit = new QLineEdit;
     hostnameLabel->setBuddy(hostnameLineEdit);
-    hostnameLineEdit->setPlaceholderText("www.example.com or 192.168.10.1");
+    if (host.isEmpty()){
+        hostnameLineEdit->setPlaceholderText("www.example.com or 192.168.10.1");
+    }
+    hostnameLineEdit->setText(host);
 
     databaseLabel = new QLabel(tr("&Database:"));
     databaseLineEdit = new QLineEdit;
     databaseLabel->setBuddy(databaseLabel);
-    databaseLineEdit->setPlaceholderText("MyDB");
+    if (dbs.isEmpty()){
+        databaseLineEdit->setPlaceholderText("MyDB");
+    }
+    databaseLineEdit->setText(dbs);
 
     usernameLabel = new QLabel(tr("&User name:"));
     usernameLineEdit = new QLineEdit;
+    if (!user.isEmpty()){
+        usernameLineEdit->setText(user);
+    }
     usernameLabel->setBuddy(usernameLabel);
 
     passwordLabel = new QLabel(tr("&Password:"));
     passwordLineEdit = new QLineEdit;
     passwordLineEdit->setEchoMode(QLineEdit::Password);
+    if (!pass.isEmpty()){
+        passwordLineEdit->setText(pass);
+    }
     passwordLabel->setBuddy(passwordLabel);
 
     QGridLayout *layout = new QGridLayout;
