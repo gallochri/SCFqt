1a27f36308218c7a8cc62158e4cf1de6b67b7a0b
Christian Galeffi <chri@gallochri.com>
2017-05-20 14:07:01 +0200

    Refactoring code

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -27,7 +27,8 @@ void MainWindow::closeEvent(QCloseEvent *event)
     res = QMessageBox::question(this, title, tr("Would you like to close?"),
                                 QMessageBox::Ok,
                                 QMessageBox::Cancel);
-    if (res == QMessageBox::Cancel){
+    if (res == QMessageBox::Cancel)
+    {
         event->ignore();
         return;
     }
@@ -86,17 +87,24 @@ bool MainWindow::lineToTestataListino( QSqlDatabase db, QString dbName,
 {
     QByteArray firstLine = file->readLine();
     QSqlQuery query = QSqlQuery(db);
-    query.prepare("INSERT INTO " + dbName + ".testata_listino VALUES ("
-                                            "?,?,?,?,?,?,?,?,?,?,?,?)");
-    int tab[2][12] = {{0,20,23,34,40,48,56,86,125,128,136,152},
-                      {20,3,11,6,8,8,30,39,3,8,16,81}};
-    for (int i=0; i<=11; i++){
+    query.prepare("INSERT INTO "
+                  + dbName +
+                  ".testata_listino VALUES "
+                  "(?,?,?,?,?,?,?,?,?,?,?,?)");
+    int tab[2][12] =
+    {
+        {0,20,23,34,40,48,56,86,125,128,136,152},
+        {20,3,11,6,8,8,30,39,3,8,16,81}
+    };
+    for (int i=0; i<=11; i++)
+    {
         QString val;
         val = QString::fromLatin1(firstLine.mid(tab[0][i],tab[1][i]));
         query.addBindValue(val);
     }
 
-    if (!query.exec()){
+    if (!query.exec())
+    {
         QMessageBox queryError;
         queryError.critical(this,
                             qApp->tr("Import error!"),
@@ -110,32 +118,37 @@ bool MainWindow::lineToTestataListino( QSqlDatabase db, QString dbName,
 bool MainWindow::linesToListinoPrezzi(QSqlDatabase db, QString dbName,
                                       QFile *file, int totalLines)
 {
-    QElapsedTimer ET;
-    ET.start();
     int lineNumber;
-    int tab[2][19] = {
+    int tab[2][19] =
+    {
         {0,3,19,32,75,80,85,90,96,97,108,119,125,128,131,132,133,141,159},
-        {3,16,13,43,5,5,5,6,1,11,11,6,3,3,1,1,8,18,18}};
-    while (!file->atEnd()) {
+        {3,16,13,43,5,5,5,6,1,11,11,6,3,3,1,1,8,18,18}
+    };
+    while (!file->atEnd())
+    {
         QByteArray encodedLine = file->readLine();
         lineNumber++;
         QTextCodec *codec = QTextCodec::codecForName("ISO 8859-15");
         QString line = codec->toUnicode(encodedLine);
         QSqlQuery query = QSqlQuery(db);
-        query.prepare("INSERT INTO " + dbName +
-                      ".listino_prezzi VALUES ("
-                      "?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
-        for (int i=0; i<=18; i++){
+        query.prepare("INSERT INTO "
+                      + dbName +
+                      ".listino_prezzi VALUES "
+                      "(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+        for (int i=0; i<=18; i++)
+        {
             QString val;
             val = QString(line.mid(tab[0][i],tab[1][i]));
             query.addBindValue(val);
         }
 
-        if (query.exec()){
+        if (query.exec())
+        {
             productImport.setText(QString("Import line %1/%2")
                                   .arg(lineNumber)
                                   .arg(totalLines));
-            if(lineNumber%100 == 0){
+            if(lineNumber%100 == 0)
+            {
                 qApp->processEvents(QEventLoop::AllEvents);
             }
         } else {
@@ -148,17 +161,14 @@ bool MainWindow::linesToListinoPrezzi(QSqlDatabase db, QString dbName,
             return false;
         }
     }
-    double micro_sec = ET.nsecsElapsed()/1000.0;
-    QString res = QString("%1 sec").arg(micro_sec/1000000.0, 0, 'f', 2);
-    timeImport.setText(QString("Time to import: %1").arg(res));
-    productImport.setText(QString("%1 lines imported.").arg(totalLines));
     return true;
 }
 
 int MainWindow::linesCounter(QFile *file)
 {
     int lineNr = 0;
-    while (!file->atEnd()){
+    while (!file->atEnd())
+    {
         QByteArray line = file->readLine();
         ++lineNr;
     }
@@ -174,13 +184,15 @@ void MainWindow::on_import_Metel_triggered()
                                                      tr("Open Metel® File"),
                                                      "",
                                                      selFilter);
-    if (metelFile.isEmpty()){
+    if (metelFile.isEmpty())
+    {
         return;
     }
 
     QFile *file = new QFile(metelFile);
 
-    if (!file->open(QIODevice::ReadOnly | QIODevice::Text)){
+    if (!file->open(QIODevice::ReadOnly | QIODevice::Text))
+    {
         qDebug() << __func__ << "Can't read file";
         return;
     }
@@ -192,15 +204,12 @@ void MainWindow::on_import_Metel_triggered()
     //Get listino Information from first line
     QByteArray firstLine = file->readLine();
     QString listinoInitial, listinoNumber, listinoDate, listinoDescription;
-
     listinoInitial = QString::fromLatin1(firstLine.mid(20,3));
     listinoNumber = QString::fromLatin1(firstLine.mid(34,6));
-    //TODO try to improve date conversion
     listinoDate = QString::fromLatin1(firstLine.mid(40,8));
-    QDate Date = QDate::fromString(listinoDate,"yyyyMMdd");
-    listinoDate = Date.toString("dd MMMM yyyy");
+    listinoDate =
+            QDate::fromString(listinoDate,"yyyyMMdd").toString("dd MMMM yyyy");
     listinoDescription = QString::fromLatin1(firstLine.mid(56,30));
-    //Count lines
     int lineNr = linesCounter(file);
 
     QString msg = QString(
@@ -218,6 +227,7 @@ void MainWindow::on_import_Metel_triggered()
             .arg(listinoNumber)
             .arg(listinoDate);
     file->close();
+
     QMessageBox metelFileInfo;
     int ret = metelFileInfo.information(
                 this,
@@ -227,19 +237,30 @@ void MainWindow::on_import_Metel_triggered()
                 QMessageBox::Cancel);
     metelFileInfo.setDefaultButton(QMessageBox::Ok);
 
-    switch (ret) {
-    case QMessageBox::Ok:{
+    switch (ret)
+    {
+    case QMessageBox::Ok:
+    {
         file->open(QIODevice::ReadOnly | QIODevice::Text);
         QSqlDatabase db = QSqlDatabase::database("viewConnection");
         QString dbName = db.databaseName();
 
         //First line to "testata_listino"
-        if (lineToTestataListino(db, dbName, file)){
+        if (lineToTestataListino(db, dbName, file))
+        {
             //The Rest file to "listino_prezzi"
-            if (linesToListinoPrezzi(db, dbName, file, lineNr)){
+            QElapsedTimer ET;
+            ET.start();
+            if (linesToListinoPrezzi(db, dbName, file, lineNr))
+            {
                 qDebug() << __func__ << "Ok, all file imported";
+                double micro_sec = ET.nsecsElapsed()/1000.0;
+                QString res = QString("%1 sec")
+                        .arg(micro_sec/1000000.0, 0, 'f', 2);
+                timeImport.setText(QString("Time to import: %1").arg(res));
+
             } else {
-                qDebug() << __func__ << "Problem at line___";
+                qDebug() << __func__ << "Problem during import.";
             }
         }
         break;
@@ -268,7 +289,8 @@ void MainWindow::on_pushButtonFinder_clicked()
                   "FROM listino_prezzi "
                   "WHERE CONCAT_WS('|',sigla_marchio,codice_prodotto,"
                   "descrizione_prodotto) LIKE '%" + word + "%'");
-    if (query.exec()){
+    if (query.exec())
+    {
         QSqlQueryModel *model = new QSqlQueryModel;
         model->setQuery(query);
         ui->tableView->setModel(model);
